245b2e7bcf96757a22a42e298fe26e020e3b3ec9
Jinhong Zhu <21972779@student.curtin.edu.au>
2025-03-16 12:39:43 +0800

    Implement the generate_code_api View (in transfer/views.py)

--- transfer/views.py
+++ transfer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .utils import generate_code
+from .models import FileShare
 
-# Create your views here.
+def generat_code_api(request):
+    if request.method == 'POST': # Only allow POST requests for code generation
+        code = generate_code()
+        file_share = FileShare(code=code) # Create a new FileShare object and set the code
+        file_share.save() # Save the object to the database
+        return JsonResponse({'code': code}) # Return the code as a JSON response
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405) # Respond with 405 Method Not Allowed for GET or other methods
