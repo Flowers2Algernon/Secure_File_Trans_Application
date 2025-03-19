ac76024eea25fa986ba1dcf8165013ba17ddbe5c
Jinhong Zhu <21972779@student.curtin.edu.au>
2025-03-19 21:27:08 +0800

    update code hash & verification logic in utils.py

--- transfer/utils.py
+++ transfer/utils.py
@@ -1,7 +1,21 @@
 import secrets
 import string
+import hashlib
+from datetime import datetime,timedelta
+from django.conf import settings
 
 def generate_code(length=6):
     alphabet = string.ascii_letters + string.digits
     code = ''.join(secrets.choice(alphabet) for i in range(length))
     return code
+
+def hash_access_code(code):
+    return hashlib.sha256(code.encode('utf-8').hexdigest())
+
+def verify_access_code(entered_code, hashed_code):
+    return hashlib.sha256(entered_code.encode('utf-8')).hexdigest()
+    # get hash code for the user entered code not compare now.
+
+def get_code_expire_time():
+    return datetime.utcnow() + timedelta(minutes=settings.ACCESS_CODE_EXPIRE_MINUTES)
+    # get the expire time for the code
